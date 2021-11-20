7df18b1e4ec7dd42c8d418fef21b7c78b22fad0d
pawornwat <ta.pawornwat_st@tni.ac.th>
2021-11-20 11:12:10 +0700

    react

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Name = () =>{
+    return(
+        <div>
+            <h1>
+                Pawornwat Tangwattanapornchai
+            </h1>
+        </div>
+    )
+}
+export default Name
